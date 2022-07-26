8d50828dc8b683ef06728021629884405f22751e
sbalayan1 <balayans2014@yahoo.com>
2022-07-26 01:59:14 -0700

    update

---
+++ loops.py
@@ -0,0 +1,5 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print (magician)
+
+#the above defines a loop. It tells python to associate the variable magician with the first value from the magicians list. Then print the name of the magician. Repeat until we've iterated over the entire list.
